4b858b6b71feac7122d43cbc4a94b04585bfc84a
MartinoySolomon <noy.kakoun@gmail.com>
2025-09-02 18:38:52 +0300

    added timing logic

--- src/components/TrafficLight/TrafficLight.tsx
+++ src/components/TrafficLight/TrafficLight.tsx
@@ -2,33 +2,40 @@ import { useEffect, useState } from "react";
 import "./TrafficLight.css";
 
 export default function TrafficLight() {
-	const [activeLight, setActiveLight] = useState("red");
-
+	const [activeLight, setActiveLight] = useState(["red"]);
+	const timer = activeLight.find((light) => light === "red")
+		? 4000
+		: activeLight.find((light) => light === "yellow")
+		? 1500
+		: 5000;
 	useEffect(() => {
-		const interval = setInterval(() => {
-			setActiveLight((prev) => {
-				if (prev === "red") return "yellow";
-				if (prev === "yellow") return "green";
-				return "red";
-			});
-		}, 2000);
-		return () => clearInterval(interval);
-	}, []);
+		const timeout = setTimeout(() => {
+			setActiveLight((prev) =>
+				prev[0] === "red"
+					? ["yellow"]
+					: prev[0] === "yellow"
+					? ["green"]
+					: ["red"]
+			);
+		}, timer);
+
+		return () => clearTimeout(timeout);
+	}, [activeLight]);
 
 	return (
 		<>
 			<div className="trafficLightBox">
 				<div
 					className={`trafficLight red ${
-						activeLight == "red" && "active"
+						activeLight.find((light) => light === "red") && "active"
 					}`}></div>
 				<div
 					className={`trafficLight yellow ${
-						activeLight == "yellow" && "active"
+						activeLight.find((light) => light === "yellow") && "active"
 					}`}></div>
 				<div
 					className={`trafficLight green ${
-						activeLight == "green" && "active"
+						activeLight.find((light) => light === "green") && "active"
 					}`}></div>
 			</div>
 			<div className="trafficLightBoxPole"></div>
